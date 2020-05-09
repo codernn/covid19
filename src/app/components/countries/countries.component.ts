1e0b8fd4b06c6b4233927af77b3e26d445a23387
codernn <schakradhar129@gmail.com>
2020-05-09 11:16:27 +0530

    charts are empty

--- src/app/components/countries/countries.component.ts
+++ src/app/components/countries/countries.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataServiceService } from 'src/app/services/data-service.service';
+import { GlobalDataSummary } from 'src/app/models/global-data';
 
 @Component({
   selector: 'app-countries',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./countries.component.css']
 })
 export class CountriesComponent implements OnInit {
-
-  constructor() { }
+  
+  totalConfirmed = 0;
+  totalActive = 0;
+  totalDeaths = 0;
+  totalRecovered = 0;
+  data : GlobalDataSummary[];
+  countries : string[] = [];
+  constructor(private service : DataServiceService) { }
 
   ngOnInit() {
+
+    this.service.getGlobalData().subscribe(result=>{
+      this.data = result;
+      this.data.forEach(cs=>{
+        this.countries.push(cs.country);
+      })
+      console.log(this.countries)
+    })
   }
 
+  updateValues(country : string){
+    console.log(country);
+    this.data.forEach(cs=>{
+      if(cs.country==country){
+        this.totalActive = cs.active;
+        this.totalConfirmed = cs.confirmed;
+        this.totalDeaths = cs.deaths;
+        this.totalRecovered = cs.recovered
+      }
+    })
+  }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataServiceService } from 'src/app/services/data-service.service';
+import { GlobalDataSummary } from 'src/app/models/global-data';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  totalConfirmed = 0;
+  totalActive = 0;
+  totalDeaths = 0;
+  totalRecovered = 0;
+  globaldata : GlobalDataSummary[];
+  constructor(private dataService : DataServiceService) { }
 
-  ngOnInit() {
+  ngOnInit(): void{
+    this.dataService.getGlobalData()
+    .subscribe(
+      {
+      next : (result)=>{
+        console.log(result);
+        this.globaldata = result;
+        result.forEach(cs=>{
+          if(!Number.isNaN(cs.confirmed)){
+            this.totalActive += cs.active;
+            this.totalConfirmed += cs.confirmed;
+            this.totalDeaths += cs.deaths;
+            this.totalRecovered += cs.recovered;
+          }
+        })
+        console.log(this.totalActive)
+      }
+    })
   }
 
 }
