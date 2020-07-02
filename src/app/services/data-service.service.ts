f4e7ee2712b94204c0daa62bc114539cccead61e
Chakradhar <49382834+codernn@users.noreply.github.com>
2020-07-02 10:23:35 +0530

    Update data-service.service.ts

--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -8,7 +8,7 @@ import { DateWiseData } from '../models/date-wise-data';
 })
 export class DataServiceService {
 
-  private globalDataUrl = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/05-09-2020.csv';
+  private globalDataUrl = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/06-30-2020.csv';
   private dateWiseDataUrl = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv';
   constructor(private http : HttpClient) {}
 
